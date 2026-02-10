ed56c1f9e76e2faa82a15b82f1af43bbde3be062
Al McElrath <hello@yrns.org>
2026-02-10 13:11:50 -0800

    use nix-fast-build for ci

--- flake.nix
+++ flake.nix
@@ -6,11 +6,22 @@
     nixpkgs,
   }: let
     forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    attrs = drv: {
+      name = drv.name;
+      value = drv;
+    };
   in {
     legacyPackages = forAllSystems (system:
       import ./default.nix {
         pkgs = import nixpkgs {inherit system;};
       });
     packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
+
+    # CI cached outputs.
+    checks = forAllSystems (system: let
+      pkgs = import nixpkgs {inherit system;};
+      cacheOutputs = (import ./ci.nix {inherit pkgs;}).cacheOutputs;
+    in
+      builtins.listToAttrs (map attrs cacheOutputs));
   };
 }
