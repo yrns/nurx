7cc1a426300383f6a39f6abec07de4c51c149639
Al McElrath <hello@yrns.org>
2026-02-27 17:55:34 -0800

    mark aarch64-linux as broken

--- pkgs/blender/default.nix
+++ pkgs/blender/default.nix
@@ -5,5 +5,7 @@
 in
   (pkgs.blender.overrideAttrs (final: prev: {
     patches = prev.patches ++ lib.optional stdenv.hostPlatform.isDarwin ./darwin.patch;
-    meta.broken = false;
+    # Darwin works.
+    # meta.broken = false;
+    meta.broken = stdenv.hostPlatform.isLinux && stdenv.hostPlatform.isAarch64; # "wayland-scanner required but not found"
   })).override {inherit sse2neon;}
