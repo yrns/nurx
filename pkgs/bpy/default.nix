42dfdb1ae1da6d7e89bbbca155bece1b1e23cdf0
Al McElrath <hello@yrns.org>
2026-06-07 17:44:15 -0700

    bpy: update python

--- pkgs/bpy/default.nix
+++ pkgs/bpy/default.nix
@@ -2,36 +2,40 @@
   blender,
   lib,
   pkg-config,
-  python311,
-  python311Packages,
+  python313,
+  python313Packages,
   stdenv,
 }:
-python311Packages.buildPythonPackage {
-  inherit (blender) version src patches postPatch preConfigure buildInputs pythonPath;
+python313Packages.buildPythonPackage {
+  inherit (blender)
+    version
+    src
+    patches
+    postPatch
+    preConfigure
+    buildInputs
+    pythonPath
+    ;
 
   pname = "bpy";
 
   # Blender has its own script to build a wheel.
   pyproject = false;
 
-  cmakeFlags =
-    blender.cmakeFlags
-    ++ [
-      "-C../build_files/cmake/config/bpy_module.cmake"
-      # (lib.cmakeBool "WITH_PYTHON_SAFETY" true)
-      (lib.cmakeBool "WITH_INSTALL_PORTABLE" true)
-      (lib.cmakeFeature "CMAKE_INSTALL_PREFIX" "${placeholder "out"}/${python311.sitePackages}")
-    ];
+  cmakeFlags = blender.cmakeFlags ++ [
+    "-C../build_files/cmake/config/bpy_module.cmake"
+    # (lib.cmakeBool "WITH_PYTHON_SAFETY" true)
+    (lib.cmakeBool "WITH_INSTALL_PORTABLE" true)
+    (lib.cmakeFeature "CMAKE_INSTALL_PREFIX" "${placeholder "out"}/${python313.sitePackages}")
+  ];
 
   # Remove wrapPython? Not sure why the Python module requires pkg-config.
-  nativeBuildInputs = blender.nativeBuildInputs ++ [pkg-config]; # needed for bpy?
+  nativeBuildInputs = blender.nativeBuildInputs ++ [ pkg-config ]; # needed for bpy?
 
   propagatedBuildInputs = blender.pythonPath;
 
-  meta =
-    builtins.removeAttrs blender.meta ["mainProgram"]
-    // {
-      description = "Blender Python module";
-      homepage = "https://builder.blender.org/download/bpy/";
-    };
+  meta = builtins.removeAttrs blender.meta [ "mainProgram" ] // {
+    description = "Blender Python module";
+    homepage = "https://builder.blender.org/download/bpy/";
+  };
 }
